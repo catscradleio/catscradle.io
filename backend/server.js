bd25d90e0ab428106abee432bba5a3fd25fa6b05
demetrios711 <demetri.sakellaropoulos@gmail.com>
2019-05-11 19:00:18 -0700

    Added a fair bit of outline, check the script section of webpack for some info. I'll add to the readme as well. Backend is where server.js (which manages mongo and express + routes will live, public has the index.html and public, src has the files that will be compiled into bundle.js)

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Simple Express React Webpack Babel Setup Environment';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
