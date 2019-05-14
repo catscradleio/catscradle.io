c06c6ed65419b379518b47ab802347369a7727c2
ndo277 <nhat.do277@gmail.com>
2019-05-14 10:19:11 -0700

    Prep for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Heroku Deployment
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 /**
  * Initialize mongoose
  */
